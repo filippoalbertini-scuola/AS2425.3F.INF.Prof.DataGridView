11b288405666db8b997a889ba1137355a5748bf2
Filippo Albertini <filippo@filippoalbertini.it>
2025-03-31 09:35:12 +0200

    feat: created matrix with random number

--- AS2425.3F.INF.Prof.DataGridView/Form1.cs
+++ AS2425.3F.INF.Prof.DataGridView/Form1.cs
@@ -20,10 +20,46 @@ namespace AS2425._3F.INF.Prof.DataGridView
 
         private void btnAssegnaValori_Click(object sender, EventArgs e)
         {
+            // crea la matrice di valori
 
+            n_righeColonne = Convert.ToInt32("0" + txtRigheColonne.Text);
+
+            if (n_righeColonne < MIN_ROWCOL_MATRICE || n_righeColonne > MAX_ROWCOL_MATRICE)
+            {
+                MessageBox.Show("Limiti matrice fuori intervallo.");
+                n_righeColonne = 0;
+                return;
+            }
+
+            // istanzia la matrice
+            valori = new int[n_righeColonne, n_righeColonne];
+
+            // assegna ad ogni valore della matrice un valore casuale (random)
+            assegnaValoriCasuali();
         }
 
+        /// <summary>
+        /// Assegna i valori casuali nella matrice
+        /// </summary>
+        private void assegnaValoriCasuali()
+        {
+            // dichiara e istanzia la variabile casuale
+            Random r = new Random();
 
+            int valore;
+
+            // posiziona i premi nel campo di gioco
+            for (int row = 0; row < n_righeColonne; row++)
+            {
+                for (int col = 0; col < n_righeColonne; col++)
+                {
+                    valore = r.Next(MIN_VALUE_MATRICE, MAX_VALUE_MATRICE + 1);
+
+                    // posizionalo nel campo di gioco
+                    valori[row, col] = valore;
+                }
+            }
+        }
 
         private void btnVisualizzaMatrice_Click(object sender, EventArgs e)
         {
