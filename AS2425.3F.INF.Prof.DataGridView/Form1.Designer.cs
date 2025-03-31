ad897874640057085ee236eb9c550b1236e74034
Filippo Albertini <filippo@filippoalbertini.it>
2025-03-31 09:18:29 +0200

    feat; UI definition

--- AS2425.3F.INF.Prof.DataGridView/Form1.Designer.cs
+++ AS2425.3F.INF.Prof.DataGridView/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            btnAssegnaValori = new Button();
+            label1 = new Label();
+            txtRigheColonne = new TextBox();
+            dgvValori = new System.Windows.Forms.DataGridView();
+            btnVisualizzaMatrice = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvValori).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnAssegnaValori);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(txtRigheColonne);
+            groupBox1.Location = new Point(33, 20);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(391, 104);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Matrice";
+            // 
+            // btnAssegnaValori
+            // 
+            btnAssegnaValori.Location = new Point(228, 33);
+            btnAssegnaValori.Name = "btnAssegnaValori";
+            btnAssegnaValori.Size = new Size(127, 47);
+            btnAssegnaValori.TabIndex = 2;
+            btnAssegnaValori.Text = "Assegna valori";
+            btnAssegnaValori.UseVisualStyleBackColor = true;
+            btnAssegnaValori.Click += btnAssegnaValori_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Righe/Colonne";
+            // 
+            // txtRigheColonne
+            // 
+            txtRigheColonne.Location = new Point(130, 43);
+            txtRigheColonne.Name = "txtRigheColonne";
+            txtRigheColonne.Size = new Size(72, 27);
+            txtRigheColonne.TabIndex = 0;
+            // 
+            // dgvValori
+            // 
+            dgvValori.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvValori.Location = new Point(35, 194);
+            dgvValori.Name = "dgvValori";
+            dgvValori.RowHeadersWidth = 51;
+            dgvValori.Size = new Size(753, 244);
+            dgvValori.TabIndex = 1;
+            // 
+            // btnVisualizzaMatrice
+            // 
+            btnVisualizzaMatrice.Location = new Point(33, 145);
+            btnVisualizzaMatrice.Name = "btnVisualizzaMatrice";
+            btnVisualizzaMatrice.Size = new Size(382, 43);
+            btnVisualizzaMatrice.TabIndex = 2;
+            btnVisualizzaMatrice.Text = "Visualizza matrice";
+            btnVisualizzaMatrice.UseVisualStyleBackColor = true;
+            btnVisualizzaMatrice.Click += btnVisualizzaMatrice_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnVisualizzaMatrice);
+            Controls.Add(dgvValori);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Prof;Lab2.11 3F; 31/03/25; Esempio utilizzo DataGridView con matrici";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvValori).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Button btnAssegnaValori;
+        private Label label1;
+        private TextBox txtRigheColonne;
+        private System.Windows.Forms.DataGridView dgvValori;
+        private Button btnVisualizzaMatrice;
     }
 }
--- AS2425.3F.INF.Prof.DataGridView/Form1.cs
+++ AS2425.3F.INF.Prof.DataGridView/Form1.cs
@@ -2,9 +2,33 @@ namespace AS2425._3F.INF.Prof.DataGridView
 {
     public partial class Form1 : Form
     {
+        // limiti della matrice
+        const int MIN_ROWCOL_MATRICE = 1;
+        const int MAX_ROWCOL_MATRICE = 20;
+
+        // limiti intervallo di valori della matrice
+        const int MIN_VALUE_MATRICE = -10;
+        const int MAX_VALUE_MATRICE = +10;
+
+        // definizione matrice con i valori generati (matrice)
+        private int[,] valori;
+        int n_righeColonne;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnAssegnaValori_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnVisualizzaMatrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
